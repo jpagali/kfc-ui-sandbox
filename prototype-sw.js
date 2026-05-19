2ebf9bf6688f9a7f45089af53bd9285e4894026b
jpagali <justinrenegade1234@gmail.com>
2026-05-19 19:27:13 +0800

    Fix prototype cart flow regressions

--- prototype-sw.js
+++ prototype-sw.js
@@ -1,56 +1,18 @@
-const CACHE_NAME = "kfc-atlas-prototype-v52";
-const CORE_ASSETS = [
-  "./",
-  "./rna-sneak-peek-prototype.html",
-  "./i18n/market-flags.js",
-  "./i18n/translations.js",
-  "./assets/splash/kfc-bucket-white.png",
-  "./manifest.webmanifest",
-  "./app-icon.svg",
-  "./app-icon-180.png",
-  "./app-icon-192.png",
-  "./app-icon-512.png"
-];
+const CACHE_NAME = "kfc-atlas-prototype-disabled-v54";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => Promise.all(CORE_ASSETS.map((asset) => cache.add(asset).catch(() => null))))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
       .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put("./rna-sneak-peek-prototype.html", copy));
-          return response;
-        })
-        .catch(() => caches.match("./rna-sneak-peek-prototype.html"))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      });
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
